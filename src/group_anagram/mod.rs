4178cdfb84fce145eb8ced3f9dc89ccacae7f9ee
mohmmedalkhatim <developmentaccun@outlook.com>
2025-02-16 21:01:47 +0200

    working on the group_anagram preblom

---
+++ src/group_anagram/mod.rs
@@ -0,0 +1,28 @@
+use std::{collections::HashMap, sync::Mutex};
+
+#[warn(dead_code)]
+pub struct Solution;
+
+impl Solution {
+    pub fn group_anagrams(list: Vec<String>) -> Vec<Vec<String>> {
+        let mut memory = HashMap::new();
+        for item in list {
+            let mut count = Vec::new();
+            for ch in item.chars()  {
+                count.push(ch as u8 - 'a' as u8);
+            }
+            memory.entry(count).or_insert(Vec::new()).push(item);
+        }
+        memory.into_values().collect()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+    #[test]
+    fn group_anagram() {
+        
+    }
+
+}
