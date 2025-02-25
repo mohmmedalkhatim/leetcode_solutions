99ddba9e51162db439f4f4f281ec0cac5df8d4fa
mohmmedalkhatim <developmentaccun@outlook.com>
2025-02-25 17:23:48 +0200

    refactor valid_anagram function for clarity and remove unused code

--- src/valid_anagram/mod.rs
+++ src/valid_anagram/mod.rs
@@ -6,32 +6,24 @@ pub struct Solution;
 impl Solution {
     #[warn(dead_code)]
     fn valid_anagram(s: String, mut t: String) -> bool {
-        let mut memory_s: HashMap<String, usize> = HashMap::new();
-        let mut memory_t: HashMap<String, usize> = HashMap::new();
-        if s.len() != t.len() {
-            return false;
-        }
-        for i in 0..s.len(){
-        }
-
-
+    
         return true;
     }
     #[warn(dead_code)]
-    fn valid_anagram_sort(s:String,t:String)->bool{
-        let mut a :Vec<&str>= s.split("").collect();
-        let mut b :Vec<&str>= t.split("").collect();
-         a.sort(); b.sort();
-         if a == b {
-             return true;
-         }else {
-             return false;
-         }
+    fn valid_anagram_sort(s: String, t: String) -> bool {
+        let mut a: Vec<&str> = s.split("").collect();
+        let mut b: Vec<&str> = t.split("").collect();
+        a.sort();
+        b.sort();
+        if a == b {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::Solution;
-
 }
