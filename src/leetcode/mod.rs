d514d6b6856160faa8aa1c2e694048c7a35ec744
mohmmedalkhatim <developmentaccun@outlook.com>
2025-02-27 21:13:26 +0200

    adding the modules and fixing misstakes in the tow_sum_2 problem

---
+++ src/leetcode/mod.rs
@@ -0,0 +1,11 @@
+mod contains_duplicate;
+mod tow_sum;
+mod valid_anagram;
+mod group_anagram;
+mod top_k_frequant_number;
+mod encode_decode;
+mod product_expect_self;
+mod vaild_palindroma;
+mod longest_consicative_number;
+mod vaild_sudoko;
+mod tow_sum_2;
--- src/leetcode/tow_sum_2/mod.rs
+++ src/leetcode/tow_sum_2/mod.rs
@@ -1,3 +1,29 @@
-fn tow_sum_2() {
-    
+use std::collections::HashMap;
+
+fn tow_sum_2(arr: Vec<i32>, target: i32) -> Vec<usize> {
+    let mut start = 0 as usize;
+    let mut end = arr.len() - 1;
+    while start < end {
+        let sum = arr[start] + arr[end];
+        if sum == target {
+            return vec![start + 1, end + 1];
+        }
+        if sum > target {
+            end -= 1;
+            continue;
+        }
+        if sum < target {
+            start += 1;
+            continue;
+        }
+    }
+    vec![1,3]
+}
+#[cfg(test)]
+mod test {
+    use super::tow_sum_2;
+    #[test]
+    fn tow_sum_2_test() {
+        assert_eq!(tow_sum_2(vec![2,7,11,12],9),vec![1,2]);
+    }
 }
