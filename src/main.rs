51132f6501ff6ed002f6b6edbfeeae023300469e
mohmmedalkhatim <developmentaccun@outlook.com>
2025-02-27 20:42:33 +0200

    moving the problem to a new model

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,6 @@
-mod contains_duplicate;
-mod tow_sum;
-mod valid_anagram;
-mod group_anagram;
-mod top_k_frequant_number;
-mod encode_decode;
-mod product_expect_self;
-mod vaild_palindroma;
-mod longest_consicative_number;
-mod vaild_sudoko;
+mod leetcode;
+
+
 fn main() {
 
     for i in (0..10).rev()  {
